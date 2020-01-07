b5bd62e5c4ff72e212e3314e32f0e141d395eac5
Oula <osiljamo@gmail.com>
2020-01-07 16:32:16 +0200

    Add simple expressjs template

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+   return res.send("TEST")
+});
+
+app.listen(process.env.PORT, () => {
+   console.log(`App listening on port ${process.env.PORT}!`)
+});
